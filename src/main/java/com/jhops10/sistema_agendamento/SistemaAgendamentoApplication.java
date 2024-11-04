12e68adc79b53e4ccdda26d3d42b4c9ffe0c6b66
João Paulo <joaowais@gmail.com>
2024-11-04 14:20:22 -0300

    Adiciona configuração do projeto e dependências

---
+++ src/main/java/com/jhops10/sistema_agendamento/SistemaAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package com.jhops10.sistema_agendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAgendamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAgendamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhops10/sistema_agendamento/SistemaAgendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhops10.sistema_agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAgendamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
